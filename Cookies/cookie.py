3f7b866da86ff4597ed32934641e9e63aec0bb3b
atul.singh <atuls4441@gmail.com>
2020-01-14 18:00:47 +0530

    Screen_Shot

---
+++ Cookies/cookie.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.braingle.com/")
+
+Cookie = {'name':'Atul', 'value':'201'}
+
+driver.add_cookie(Cookie)
+print("Cookie has been added")
+sleep(2)
+cookie = driver.get_cookie("Atul")
+print(cookie['value'])
+driver.quit()
